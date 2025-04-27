e18d9af412de5ecd1d41d8900794a58ea0f3d086
Krystian <milanello07@gmail.com>
2025-04-27 11:15:23 +0200

    another commit

--- src/main/java/com/company/enroller/persistence/MeetingService.java
+++ src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -1,7 +1,9 @@
 package com.company.enroller.persistence;
 
 import com.company.enroller.model.Meeting;
+import com.company.enroller.model.Participant;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
 
@@ -20,6 +22,28 @@ public class MeetingService {
 		String hql = "FROM Meeting";
 		Query query = this.session.createQuery(hql);
 		return query.list();
+	}
+
+	public Meeting findById(Long id) {
+		return session.getSession().get(Meeting.class, id);
+	}
+
+	public Meeting add(Meeting meeting) {
+		Transaction transaction = session.getSession().beginTransaction();
+		session.getSession().save(meeting);
+		transaction.commit();
+		return meeting;
+	}
+	public void update(Meeting meeting) {
+		Transaction transaction = session.getSession().beginTransaction();
+		session.getSession().merge(meeting);
+		transaction.commit();
+	}
+
+	public void delete(Meeting meeting) {
+		Transaction transaction = session.getSession().beginTransaction();
+		session.getSession().delete(meeting);
+		transaction.commit();
 	}
 
 }
